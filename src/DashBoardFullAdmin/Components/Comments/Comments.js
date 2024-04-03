7141c8415cae8ba1b739a1abc23104c7a5c37789
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-04-03 23:46:45 +0330

    Create File Add Codeing DashBoardFullAdmin ✌️

---
+++ src/DashBoardFullAdmin/Components/Comments/Comments.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ErrorBox from '../Errorbox/Errorbox'
+
+export default function Comments() {
+  return (
+    <ErrorBox msg="هیچ کامنتی یافت نشد" />
+  )
+}
---
+++ src/DashBoardFullAdmin/Components/Products/Products.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ErrorBox from '../Errorbox/Errorbox'
+
+export default function Products() {
+  return (
+    <ErrorBox msg="هیچ محصولی یافت نشد" />
+  )
+}
