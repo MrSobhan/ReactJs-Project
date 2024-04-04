18708f23eafa81bab435d4c317c46eea3f4b15da
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-04-04 21:24:50 +0330

    Change Index Root (ReactDOM)

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./DashBoardFullAdmin/App";
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
-const root = ReactDOM.createRoot(document.querySelector("body"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
