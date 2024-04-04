2d51e265847eec70a7c339b4a9c9ffcbf961cce2
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-04-04 21:51:54 +0330

    Add Backend New File

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ import { BrowserRouter } from "react-router-dom";
 // import App from "./menu-project/App";
 // import App from "./projects/ContextApi/App";
 // import App from "./ProductShop/App";
-import App from "./DashBoardFullAdmin/App";
+import App from "./DashBoardFullAdmin/frontend/App";
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 const root = ReactDOM.createRoot(document.getElementById("root"));
