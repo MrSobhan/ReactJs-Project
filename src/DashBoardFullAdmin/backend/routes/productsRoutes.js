a4f14a4c8bc5bfa86d5003e5f960d142f997ea25
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-04-10 20:16:41 +0330

    Reverse Arry Product in Node Js

--- src/DashBoardFullAdmin/backend/routes/productsRoutes.js
+++ src/DashBoardFullAdmin/backend/routes/productsRoutes.js
@@ -7,7 +7,7 @@ const productsRouter = express.Router();
 
 productsRouter.get("/", (req, res) => {
   console.log('get products');
-  let selectAllProductsQuery = `SELECT * FROM Products`;
+  let selectAllProductsQuery = 'SELECT * FROM `products` ORDER BY `id` DESC';
   SabzlearnShopDB.query(selectAllProductsQuery, (err, result) => {
     console.log('get products query');
     if (err) {
