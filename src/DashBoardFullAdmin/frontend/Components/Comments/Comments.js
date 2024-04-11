18ab451e5df84fc557c3cd6c91bcc22a184e1e15
MrSobhan <sobhan.mosazadeh85@gmail.com>
2024-04-11 19:59:59 +0330

    DeleteModal & DetailsModal Handeler

--- src/DashBoardFullAdmin/frontend/Components/Comments/Comments.js
+++ src/DashBoardFullAdmin/frontend/Components/Comments/Comments.js
@@ -1,12 +1,120 @@
-import React from 'react'
-import DeleteModal from '../DeleteModal/DeleteModal'
-import ErrorBox from '../Errorbox/Errorbox'
+import React, { useEffect, useState } from "react";
+import ErrorBox from "../Errorbox/Errorbox";
+import "../ProductsTable/ProductsTable.css";
+
+import DeleteModal from "../DeleteModal/DeleteModal";
+import DetailsModal from "../DetailsModal/DetailsModal";
+import EditModal from "../EditModal/EditModal";
 
 export default function Comments() {
+  const [allComments, setAllComments] = useState([]);
+  const [commentID, setCommentID] = useState(null);
+  const [commentText, setCommentText] = useState("");
+
+  const [isShowDeleteModal, setIsShowDeleteModal] = useState(false);
+  const [isShowDetailsModal, setIsShowDetailsModal] = useState(false);
+  const [isShowEditModal, setIsShowEditModal] = useState(false);
+
+  const getAllComment = () => {
+    fetch("http://localhost:8000/api/comments")
+      .then((res) => res.json())
+      .then((comments) => setAllComments(comments));
+  };
+
+  useEffect(() => {
+    getAllComment();
+  }, []);
+
+  const deleteModalCancelAction = () => {
+    console.log("مدال حذف کنسل شد");
+    setIsShowDeleteModal(false);
+  };
+
+  const deleteModalSubmitAction = () => {
+    console.log("مدال حذف تایید شد");
+    fetch(`http://localhost:8000/api/comments/${commentID}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setIsShowDeleteModal(false);
+        getAllComment();
+      });
+  };
+
+  const closeDetailsmodal = () => {
+    setIsShowDetailsModal(false);
+    console.log("مدال جزییات بسته شد");
+  };
+
   return (
-    <>
-    <ErrorBox msg="هیچ کامنتی یافت نشد" />
-    {/* <DeleteModal /> */}
-    </>
-  )
+    <div className="cms-main">
+      {allComments.length ? (
+        <table className="products-table">
+          <thead>
+            <tr className="products-table-heading-tr">
+              <th>اسم کاربر</th>
+              <th>محصول</th>
+              <th>کامنت</th>
+              <th>تاریخ</th>
+              <th>ساعت</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {allComments.map((comment) => (
+              <tr key={comment.id} className="products-table-tr">
+                <td>{comment.userID}</td>
+                <td>{comment.productID}</td>
+                <td>
+                  <button
+                    className="products-table-btn"
+                    onClick={() => {
+                      setIsShowDetailsModal(true);
+                      setCommentText(comment.body);
+                    }}
+                  >
+                    دیدن متن
+                  </button>
+                </td>
+                <td>{comment.date}</td>
+                <td>{comment.hour}</td>
+                <td>
+                  <button
+                    className="products-table-btn"
+                    onClick={() => {
+                      setIsShowDeleteModal(true);
+                      setCommentID(comment.id);
+                    }}
+                  >
+                    حذف
+                  </button>
+                  <button className="products-table-btn">ویرایش</button>
+                  <button className="products-table-btn">پاسخ</button>
+                  <button className="products-table-btn">تایید</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <ErrorBox msg="هیچ کامنتی یافت نشد" />
+      )}
+
+      {/* Modal */}
+
+      {isShowDeleteModal && (
+        <DeleteModal
+          submitAction={deleteModalSubmitAction}
+          cancelAction={deleteModalCancelAction}
+        />
+      )}
+
+      {isShowDetailsModal && (
+        <DetailsModal onHide={closeDetailsmodal}>
+          <h3>{commentText}</h3>
+        </DetailsModal>
+      )}
+    </div>
+  );
 }
