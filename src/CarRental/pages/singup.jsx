777a6ec61e9f33f1b830493bf6becc69ab38c57f
MrSobhan <sobhan.mosazadeh85@gmail.com>
2025-03-12 21:59:25 +0330

    Bg Style login & singup

--- src/CarRental/pages/singup.jsx
+++ src/CarRental/pages/singup.jsx
@@ -16,12 +16,15 @@ import { FcGoogle } from "react-icons/fc";
 function Singup() {
     return (
         <section className="px-8">
+            <div className="absolute inset-x-0 top-20 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+                <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}></div>
+            </div>
             <div className="container mx-auto h-screen grid place-items-center">
                 <Card
                     shadow={false}
-                    className="md:px-24 py-8 border border-gray-300"
+                    className="md:px-24 py-8 bg-transparent"
                 >
-                    <CardHeader shadow={false} floated={false} className="text-center">
+                    <CardHeader shadow={false} floated={false} className="text-center bg-transparent">
                         <div className="flex items-center justify-center">
                             <img src="./logoBrandCarRental.ico" alt="Logo_img" className="w-9 h-9" />
                             <Link to={'/'}>
