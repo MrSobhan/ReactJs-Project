90d3eb6633790a73683f2fc566421706477da051
MrSobhan <sobhan.mosazadeh85@gmail.com>
2025-05-02 17:22:37 +0330

    Change ReminderAlert UI

--- src/CarRental/Components/ReminderAlert/ReminderAlert.jsx
+++ src/CarRental/Components/ReminderAlert/ReminderAlert.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { IoClose } from "react-icons/io5";
 
 export default function ReminderAlert() {
   const today = new Date();
   const day = today.getDate();
 
-  const isVisible = day >= 20 && day <= 23;
+  const [isVisible, setIsVisible] = useState(day >= 20 && day <= 23)
 
-  if (!isVisible) return null;
 
   return (
-    <div className="w-full bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mb-4 absolute left-0 top-0 z-[99999999]">
-      <div className="flex items-center justify-between">
-        <div>
-          <p className="font-semibold">یادآوری مهم</p>
-          <p className="text-sm mt-1">لطفاً وضعیت دیتابیس را بررسی و در صورت نیاز به‌روزرسانی کنید.</p>
-        </div>
-        <div className="ml-4 text-yellow-500 text-xl">⚠️</div>
-      </div>
-    </div>
+    <>
+      {
+        isVisible && (
+          <div className="w-full bg-yellow-100 border-l-4 border-yellow-500 text-yellow-900 p-4 mb-4 absolute left-0 top-0 z-[99999999]">
+            <div className="flex items-center justify-between">
+              <div>
+                <div className="flex">
+                  <div className="ml-2 text-yellow-500 text-xl">⚠️</div>
+                  <p className="font-semibold lalezar text-lg">یادآوری مهم</p>
+                </div>
+                <p className="text-xs md:text-md mt-2">لطفاً وضعیت دیتابیس را بررسی و در صورت نیاز به‌روزرسانی کنید.</p>
+              </div>
+              <IoClose className="md:ml-4 text-3xl cursor-pointer" onClick={() => setIsVisible(false)} />
+            </div>
+          </div>
+        )
+      }
+    </>
   );
 }
